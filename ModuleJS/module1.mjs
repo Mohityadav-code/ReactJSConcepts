fe4f7907ab38e4adde18b49165bcabe97635a564
Mohityadav-code <mohityadav3552@gmail.com>
2023-01-24 18:38:21 +0530

    created module folder

---
+++ ModuleJS/module1.mjs
@@ -0,0 +1,8 @@
+const a="mohit"
+const b="anjani"
+const c="mohit2"
+const d="anjani2"
+export default c
+export {a}
+export {b}
+export {d}
---
+++ ModuleJS/module2.mjs
@@ -0,0 +1,5 @@
+import UI,{a,b,d} from "./module1.mjs"
+console.log(UI);
+console.log(a);
+console.log(b);
+console.log(d);
